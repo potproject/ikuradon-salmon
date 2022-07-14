34bac857bd35445ad06b25b681e8b21066b071b2
potproject <pptppctt@gmail.com>
2022-07-14 20:34:36 +0900

    ...

--- rfc8188/rfc8188.go
+++ rfc8188/rfc8188.go
@@ -19,7 +19,7 @@ func Decrypt(body []byte, public []byte, private []byte, authSecret []byte) ([]b
 	// Create ECDH Public
 
 	p256 := elliptic.P256()
-	pubX, pubY := p256.ScalarBaseMult(keyid)
+	pubX, pubY := elliptic.Unmarshal(p256, idlen)
 	sX, _ := p256.ScalarMult(pubX, pubY, private)
 	sharedSecret := sX.Bytes()
 
